6ba4fc0fae680a1d68e85be9675fc7451fd360d7
Juan M. Rebisso <46874666+stonemantlearg@users.noreply.github.com>
2021-09-21 12:09:01 -0300

    ejercicio nuevo
    tdutime = time + date  + users + uptime

---
+++ #tdutime.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+LOGFILE=test.log
+#Crea un logfile llamado "test.log"
+
+echo "---------------------" 1>>$LOGFILE
+uptime 1>>$LOGFILE
+#Muestra el tiempo encendido del equipo y el total de usuarios conectados
+echo "---------------------"
+echo "Uptime enviado a test.log"
+echo "---------------------" 1>>$LOGFILE
+who 1>>$LOGFILE
+#Muestra los usuarios conectados y la hora de conexión
+echo "---------------------"
+echo "Who enviado a test.log"
+echo "---------------------" 1>>$LOGFILE
+date 1>>$LOGFILE
+#Muestra fecha y hora
+echo "---------------------"
+echo "Uptime enviado a test.log"
+echo "---------------------" 1>>$LOGFILE
