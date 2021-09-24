7c9c6a0bf56c01c74d11a518ab1cf37d9b753edd
Juan M. Rebisso <46874666+stonemantlearg@users.noreply.github.com>
2021-09-24 12:09:01 -0300

    ejercicio loop

---
+++ for-loop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for comida in Milanga Fideos Asado Churrasco Guiso 
+do
+    echo $comida 
+done
+
+# Dado que está situado de esa forma el loop mostrará cada comida en una linea.
+
+for entrada in "Salame Queso Bondiola Doritos"
+do
+    echo $entrada
+done
+
+echo; echo "Esto no parece muy saludable."
+
+exit 0
